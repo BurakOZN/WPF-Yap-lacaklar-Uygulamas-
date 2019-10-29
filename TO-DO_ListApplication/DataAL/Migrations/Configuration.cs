7ed1baaf6ab9af11baa63b3e8280086b623ef534
BurakOZN <burakozn@gmail.com>
2019-10-29 12:54:51 +0300

    Context nesnesi ve ilişkilerin ayarlanması.

---
+++ TO-DO_ListApplication/DataAL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DataAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAL.ToDoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DataAL.ToDoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
