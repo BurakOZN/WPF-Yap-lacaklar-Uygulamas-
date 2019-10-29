8b66cc5fb19ae2b6a5683e27c2c10ed318c4a081
BurakOZN <burakozn@gmail.com>
2019-10-29 12:55:33 +0300

    Context

---
+++ TO-DO_ListApplication/DataAL/ToDoContext.cs
@@ -0,0 +1,37 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAL
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext():base("MyContext")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder mb)
+        {
+            mb.Entity<User>().HasIndex(x => x.Id);
+            mb.Entity<ToDoList>().HasIndex(x => x.Id);
+            mb.Entity<Item>().HasIndex(x => x.Id);
+
+            mb.Entity<Item>()
+                .HasRequired(x => x.ToDoList)
+                .WithMany(x => x.Items)
+                .HasForeignKey(x => x.ToDoListId)
+                .WillCascadeOnDelete(true);
+            mb.Entity<ToDoList>()
+                .HasRequired(x => x.User)
+                .WithMany(x => x.ToDoLists)
+                .HasForeignKey(x => x.UserId)
+                .WillCascadeOnDelete(true);
+
+            base.OnModelCreating(mb);
+        }
+    }
+}
